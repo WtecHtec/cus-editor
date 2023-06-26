db75fe2e107801774612eb68084fbb38fab07a65
herzshen <herzshen@tencent.com>
2023-06-26 16:23:09 +0800

    输入

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cus-editor
 h5简单富文本编辑器
+
+
+## 定义数据格式
+每个字符为一个token
+```
+{
+  x,y,width,height,value, style
+}
+```
--- demo/main.js
+++ demo/main.js
@@ -7,17 +7,26 @@
 //   MASTER_LOG_FILE='mysql-bin',
 //   MASTER_LOG_POS=1;
 
-
 const gCtxList = [];
 const gPageList = [];
 const gPageContainer = $('.ce-page-container')
+const gWidth = 794;
+const gHeight = 900;
+const gdiff = 10
+const gCache = {
+  start: 0,
+  end: 0,
+  cacheEnd: 0,
+  size: 0,
+}
 
+let gInputing = false
 let gEventPos = {}
 let gCursorDom = null
 let gAgentCursorDom = null
 let gCurCtx = null
+let gElementList = [];
 
-let elementList = [];
 
 function _initPageContext(ctx) {
   const dpr = window.devicePixelRatio
@@ -29,11 +38,9 @@ function _initPageContext(ctx) {
 }
 
 function _createPage(pageNo = 0) {
-  const width = 794;
-  const height = 900;
   const canvas = document.createElement('canvas')
-  canvas.style.width = `${width}px`
-  canvas.style.height = `${height}px`
+  canvas.style.width = `${gWidth}px`
+  canvas.style.height = `${gHeight}px`
   canvas.style.display = 'block'
   canvas.style.backgroundColor = '#ffffff'
   // canvas.style.marginBottom = `${this.getPageGap()}px`
@@ -41,8 +48,8 @@ function _createPage(pageNo = 0) {
   gPageContainer.append(canvas)
   // 调整分辨率
   const dpr = window.devicePixelRatio
-  canvas.width = width * dpr
-  canvas.height = height * dpr
+  canvas.width = gWidth * dpr
+  canvas.height = gHeight * dpr
   canvas.style.cursor = 'text'
   const ctx = canvas.getContext('2d')
   // 初始化上下文配置
@@ -54,26 +61,34 @@ function _createPage(pageNo = 0) {
 }
 
 function _render() {
-  const len = elementList.length;
+  gCurCtx.clearRect(0, 0, gWidth, gHeight)
+  const len = gElementList.length;
   for (let i = 0; i < len; i++) {
-    const elm = elementList[i]
-    const { type, value, size, style: font } = elm
+    const elm = gElementList[i]
+    const { type, value, size, style: font, x, y, width } = elm
     gCurCtx.save()
     if (type === 'text') {
       gCurCtx.fillStyle = '#000'
       gCurCtx.font = `${size * 1}px ${font}`
-      const { width } = gCurCtx.measureText(value)
-      console.log('width===', width)
-      // gCurCtx.fillText(value, 10, 10)
+      // const { width } = gCurCtx.measureText(value)
+      // console.log('width===', width)
+      gCursorDom.style.left = `${ x + width + gdiff }px`;
+      gAgentCursorDom.style.left = `${ x + width + gdiff }px`;
+      gCurCtx.fillText(value, x, y)
     }
     gCurCtx.restore()
   }
 }
 
+function getPosition(offsetX, offsetY) {
+  if (gElementList.length === 0) return  { offsetX, offsetY }
+}
+
 function _canvasEvent() {
   gPageContainer.on('mousedown', (event) => {
-    const { offsetX, offsetY } = event;
-    console.log('mousedown===', event, offsetX, offsetY)
+    const { offsetX: eventOffsetX, offsetY: eventoffsetY } = event;
+    console.log('mousedown===', event, eventOffsetX, eventoffsetY)
+    const { offsetX, offsetY } = getPosition(eventOffsetX, eventoffsetY)
     gEventPos = {
       offsetX, offsetY
     }
@@ -116,6 +131,16 @@ function drawCursorAgent(cursorLeft, cursorTop) {
     gAgentCursorDom.autocomplete = 'off'
     gAgentCursorDom.classList.add('ce-inputarea')
     gAgentCursorDom.oninput = debounce(_input.bind(), 0)
+    gAgentCursorDom.addEventListener('compositionstart', (e) => {
+      console.log('compositionstart====', e, gEventPos)
+      if (!gInputing) {
+        gInputing = true
+      }
+    })
+    gAgentCursorDom.addEventListener('compositionend', (e) => {
+      console.log('compositionend====', e)
+      gInputing = false
+    })
   } else {
     gAgentCursorDom.style.display = 'block'
   }
@@ -147,20 +172,32 @@ function _input(e) {
     gCurCtx.fillStyle = '#000'
     gCurCtx.font = `${obj.size}px ${obj.font}`
     const { width } = gCurCtx.measureText(text)
-  
     const box = {
       width,
-      x: offsetX + lastWidth,
-      y: offsetY,
+      x: offsetX + lastWidth + gdiff,
+      y: offsetY + obj.size,
       height: obj.size
     }
-    lastWidth =  lastWidth + width
-    console.log('width===', text, width, lastWidth)
+    lastWidth =  lastWidth + width + gdiff
+    // console.log('width===', text, width, lastWidth)
     return { ...obj, ...box }
   })
-  elementList = [...elementList, ...datas]
-  console.log('_input ', elementList)
+  const { start, cacheEnd, size } = gCache
+  console.log('cacheEnd===', cacheEnd)
+  if (start === 0 || start === size) gElementList.splice( start, cacheEnd, ...datas)
+  gCache.cacheEnd = datas.length;
+  console.log('_input ', gElementList)
   _render()
+  if (!gInputing) {
+    gCache.start = gElementList.length
+    gCache.cacheEnd = 0
+    gCache.size = gElementList.length
+    const { x, width } = datas[datas.length - 1]
+    gEventPos = {
+      offsetX: x +　width ,
+      offsetY,
+    }
+  }
 }
 
 
