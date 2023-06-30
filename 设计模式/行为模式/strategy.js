4844d8c2d82f3b7bac865d7b671707dde6795aa7
herzshen <herzshen@tencent.com>
2023-06-30 23:44:55 +0800

    命令

--- 设计模式/行为模式/strategy.js
+++ 设计模式/行为模式/strategy.js
@@ -1,37 +1,37 @@
 /**
  * 策略模式
- * 强调的是行为的灵活切换（字典调用）, 通过接入行为实现灵活的实现方式
+ * 强调的是行为的灵活切换, 通过添加策略行为实现灵活的实现方式
  * 例如： 计算
  */
 
 // 接口
-class Strategy  {
-  calculate(a, b) { console.log(a,b) }
+class Strategy {
+	calculate(a, b) { console.log(a, b) }
 }
 
 class Add extends Strategy {
-  constructor() { super() }
-  calculate(a, b) { console.log(a + b); return a + b }
+	constructor() { super() }
+	calculate(a, b) { console.log(a + b); return a + b }
 }
 
 class Mod extends Strategy {
-  constructor() { super() }
-  calculate(a, b) { console.log(a % b); return a % b }
+	constructor() { super() }
+	calculate(a, b) { console.log(a % b); return a % b }
 }
 
 class Calculator {
 
-  constructor() {
-    this.strategy = null
-  }
+	constructor() {
+		this.strategy = null
+	}
 
-  setStrategy(strategy) {
-    this.strategy = strategy
-  }
+	setStrategy(strategy) {
+		this.strategy = strategy
+	}
 
-  getRuleData(a, b) {
-    this.strategy.calculate(a, b)
-  }
+	getRuleData(a, b) {
+		this.strategy.calculate(a, b)
+	}
 
 }
 
@@ -39,9 +39,9 @@ class Calculator {
 const calculator = new Calculator()
 
 calculator.setStrategy(new Add())
-calculator.getRuleData(45,12)
+calculator.getRuleData(45, 12)
 
 calculator.setStrategy(new Mod())
-calculator.getRuleData(4,2)
+calculator.getRuleData(4, 2)
 
 
